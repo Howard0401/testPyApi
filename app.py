6c0fb230ddf939adff223b6026ce58335011e3ed
TzuHaoChenVSNB <chenhoward0401@gmail.com>
2020-01-10 11:32:00 +0800

    make it better

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ class Spider():
         global s
         s = table.to_json(orient='records', force_ascii=False)
         
-@app.route('/test', methods=['POST'])
+@app.route('/', methods=['POST'])
 def test():
     data=request.get_json()
     global name     
